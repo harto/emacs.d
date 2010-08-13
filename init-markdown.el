bf01cb7806998aadd0c5fc70bcca6ff8209e96c2
Stuart Campbell <stuart@harto.org>
2010-08-13 11:07:41 +1000

    attempt to fix markdown mode

--- init-markdown.el
+++ init-markdown.el
@@ -1,4 +1,4 @@
 ;; Markdown
 
-(auto-load 'markdown-mode "~/.emacs.d/vendor/markdown-mode/markdown-mode")
+(autoload 'markdown-mode "~/.emacs.d/vendor/markdown-mode/markdown-mode")
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
