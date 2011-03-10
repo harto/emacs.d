34be919e3be6dba916258b1dffec4c94a65683bf
Stuart Campbell <stuart@harto.org>
2011-03-10 11:49:08 +1100

    use newer js2-mode

--- init-web.el
+++ init-web.el
@@ -17,8 +17,12 @@
 
 ;; JavaScript
 
+(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (setq js2-bounce-indent-p t)
 (setq js2-cleanup-whitespace t)
+(setq js2-consistent-level-indent-inner-bracket-p t)
+(setq js2-use-ast-for-indentation-p t)
 
 ;; CSS
 
