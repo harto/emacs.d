47785374291d757962511d1f248e72042ef7f36c
Stuart Campbell <stuart@harto.org>
2011-06-30 17:34:33 +1000

    more JS helpers

--- init-web.el
+++ init-web.el
@@ -24,22 +24,56 @@
 (setq js2-consistent-level-indent-inner-bracket-p t)
 (setq js2-use-ast-for-indentation-p t)
 
-(defun jslint ()
+(defun js2-jslint ()
   (interactive)
   (let ((fname (buffer-file-name)))
     (when fname
       (shell-command (format "jslint %s" fname)))))
 
-(defun js2-declare-jslinted-externs ()
-  "Use JSLint /*global ... */ declarations to define js2-additional-externs"
+(defvar js2-jslint-imports
+  "/\\*global \\([^*]+\\)\\*/")
+
+(defun trim (s)
+  (replace-regexp-in-string "^[[:blank:]\n]+\\|[[:blank:]\n]+$" "" s))
+
+(defun js2-find-jslint-imports ()
+  "Returns a list of JSLint imports as declared in a /*global ... */ block."
   (save-excursion
     (goto-char (point-min))
-    (when (re-search-forward "/\\*global \\([^*]+\\)\\*/" nil t)
-      (setq js2-additional-externs
-            (append (split-string (match-string-no-properties 1) "[ \n\t]*,[ \n\t]*")
-                    js2-additional-externs)))))
+    (when (re-search-forward js2-jslint-imports nil t)
+      (mapcar #'trim (split-string (match-string-no-properties 1) ",")))))
 
-(add-hook 'js2-mode-hook 'js2-declare-jslinted-externs)
+(defun js2-declare-jslint-imports ()
+  "Use JSLint /*global ... */ declarations to define js2-additional-externs"
+  (setq js2-additional-externs (append (js2-jslint-imports) js2-additional-externs)))
+
+(defun js2-organise-jslint-imports ()
+  "Make /*global ...*/ declarations look nice."
+  (interactive)
+  (when-let (imports (js2-find-jslint-imports))
+    (sort imports 'string<)
+    (save-excursion
+      (goto-char (point-min))
+      (re-search-forward js2-jslint-imports)
+      (replace-match "/*global ")
+      (dolist (import imports)
+        (when (>= (+ (length import) (current-column)) 80)
+          (delete-char -1)              ;delete trailing " "
+          (insert "\n  "))
+        (insert import ", "))
+      (delete-char -2)                  ;delete trailing ", "
+      (insert " */"))))
+
+(add-hook 'js2-mode-hook
+          (lambda ()
+            ;; Declare imports on load and save
+            (js2-declare-jslint-imports)
+            (add-hook 'after-save-hook
+                      'js2-declare-jslint-imports
+                      nil t)
+            ;; FIXME: key mappings
+            (define-key js2-mode-map (kbd "C-c l") 'js2-jslint)
+            (define-key js2-mode-map (kbd "C-c o") 'js2-organise-jslint-imports)))
 
 ;; CoffeeScript
 
