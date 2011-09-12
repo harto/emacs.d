ef48bd36288baa6a4cf7467db34d183987a52742
Stuart Campbell <stuart@harto.org>
2011-09-12 20:01:44 +1000

    rename directory-hooks -> directory-hooks-alist

--- tools.el
+++ tools.el
@@ -12,7 +12,8 @@
 ;; Project utilities
 
 (defun project-directory ()
-  (directory-file-name (ffip-project-root)))
+  (when-let (root (ffip-project-root))
+    (directory-file-name root)))
 
 (setq project-todo-pattern "\\b(TODO|FIXME|XXX)\\b")
 
@@ -25,10 +26,10 @@
 
 (defun project-make ()
   (interactive)
-  (let ((dir (project-directory)))
-    (if dir
-        (shell-command (format "cd %s && make" dir))
-      (message "No parent project found"))))
+  (let ((root (locate-dominating-file (buffer-file-name) "Makefile")))
+    (if root
+        (shell-command (format "cd %s && make" root))
+      (message "No Makefile found"))))
 
 (global-set-key (kbd "C-c t") 'project-todo)
 (global-set-key (kbd "C-c m") 'project-make)
@@ -36,10 +37,10 @@
 ;; Allow hooks to be defined in .dir-locals.el
 
 (defun project-apply-directory-hooks ()
-  "Sets directory-local hooks using the value of `directory-hooks', which is a
-   list of (hook-name hook-function) pairs."
-  (when (boundp 'directory-hooks)
-    (dolist (hook-definition directory-hooks)
+  "Sets directory-local hooks using the value of `directory-hooks-alist', which
+   is a list of (hook-name . hook-function) pairs."
+  (when (boundp 'directory-hooks-alist)
+    (dolist (hook-definition directory-hooks-alist)
       (add-hook (car hook-definition)
                 (cdr hook-definition)
                 nil t))))
