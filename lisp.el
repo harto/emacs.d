4019d2fedeb899701e21a4951ee6d38f0dee7079
Stuart Campbell <stuart@harto.org>
2012-04-09 19:51:10 +1000

    Disable SLIME in ClojureScript clojure-mode buffers

--- lisp.el
+++ lisp.el
@@ -8,6 +8,13 @@
 (add-to-list 'auto-mode-alist '("\\.cljs?$" . clojure-mode))
 (add-hook 'clojure-mode-hook (lambda () (paredit-mode +1)))
 
+;; ClojureScript doesn't work with SLIME. When we evaluate cljs forms they
+;; should be sent to the inferior-lisp REPL instead.
+(defadvice slime-mode (around cljs-disable-slime)
+  "Activates `slime-mode' iff the current buffer isn't a .cljs file."
+  (when (not (string-match "\\.cljs$" (buffer-name)))
+    ad-do-it))
+
 ;;; Elisp
 
 (add-hook 'emacs-lisp-mode-hook
