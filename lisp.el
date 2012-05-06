4777973729e22c6914930a4158d1c3dc05b02d86
Stuart Campbell <stuart@harto.org>
2012-05-06 13:09:00 +1000

    Add keyboard shortcut for `clojure-jack-in`

--- lisp.el
+++ lisp.el
@@ -4,15 +4,23 @@
 
 ;;; Clojure / ClojureScript
 
+(defun cljs-buffer-p ()
+  "Test if current buffer is a ClojureScript buffer."
+  (string-match "\\.cljs$" (buffer-name)))
+
 (require 'clojure-mode)
 (add-to-list 'auto-mode-alist '("\\.cljs?$" . clojure-mode))
-(add-hook 'clojure-mode-hook (lambda () (paredit-mode +1)))
+(add-hook 'clojure-mode-hook
+          (lambda ()
+            (paredit-mode +1)
+            (when (not (cljs-buffer-p))
+              (define-key clojure-mode-map (kbd "C-c C-j") 'clojure-jack-in))))
 
 ;; ClojureScript doesn't work with SLIME. When we evaluate cljs forms they
 ;; should be sent to the inferior-lisp REPL instead.
 (defadvice slime-mode (around cljs-disable-slime)
   "Activates `slime-mode' iff the current buffer isn't a .cljs file."
-  (when (not (string-match "\\.cljs$" (buffer-name)))
+  (when (not (cljs-buffer-p))
     ad-do-it))
 
 (setq slime-kill-without-query-p t)
