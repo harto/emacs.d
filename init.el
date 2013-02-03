406100561f56cdee42f664baa6de2096c81813e2
Stuart Campbell <stuart@harto.org>
2013-02-03 23:20:04 +1100

    Recognise full .markdown extension

--- init.el
+++ init.el
@@ -99,6 +99,7 @@
 ;; Other file/mode associations
 (autoload 'markdown-mode "markdown-mode.el")
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
 
 ;; Git
 (require 'magit)
