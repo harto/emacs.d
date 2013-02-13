e2f11a51d94c78b44f9d9f9d4089d017f9a931e8
Stuart Campbell <stuart@harto.org>
2013-02-13 17:23:53 +1100

    Fix color theme in console

--- init.el
+++ init.el
@@ -24,6 +24,8 @@
 ;; Appearance
 (require 'color-theme)
 (require 'color-theme-solarized)
+(unless (display-graphic-p)
+  (setq solarized-termcolors 256))
 (color-theme-solarized-dark)
 (if (boundp 'scroll-bar-mode)
     (scroll-bar-mode -1))
