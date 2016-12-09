2aa8076bcaca1fa221262950e0083094abd89a8d
Stuart Campbell <stuart@harto.org>
2016-12-09 09:42:59 -0800

    Workaround sql prompt bug in Emacs 25

--- init.el
+++ init.el
@@ -178,3 +178,11 @@
 ;; Work things
 (if (file-exists-p "~/spot/spot.el")
     (load-library "~/spot/spot"))
+
+;; Workaround https://debbugs.gnu.org/cgi/bugreport.cgi?bug=22596
+;; (fixed in Emacs 26.1)
+(require 'sql)
+;; FIXME: figure out the right hook to do this lazily
+(sql-set-product-feature 'postgres
+                         :prompt-regexp
+                         "^[[:alpha:]_]*=[#>] ")
