dd4d348ec20eabf7c10e62a7e8a30bf6e79ed598
Stuart Campbell <stuart@harto.org>
2017-09-14 20:11:57 -0700

    `cleanup-buffers'

--- lisp/utils.el
+++ lisp/utils.el
@@ -42,3 +42,25 @@ Passes arg N to `open-line'."
       (beginning-of-line)
       (delete-horizontal-space)
       (indent-for-tab-command))))
+
+(defun cleanup-buffers ()
+  "Kill as many buffers as possible."
+  (interactive)
+  (dolist (buf (buffer-list))
+    (if (auto-killable-buffer? buf)
+      (kill-buffer buf))))
+
+(defun auto-killable-buffer? (buffer-or-name)
+  (let* ((buf (get-buffer buffer-or-name))
+         (buf-name (buffer-name buf))
+         (buf-mode (with-current-buffer buf major-mode))
+         (buf-process (get-buffer-process buf)))
+    (or (and (string-match "^\\*.+" buf-name)
+             (not (equal "*scratch*" buf-name))
+             ;; per `process-kill-buffer-query-function'
+             (or (not buf-process)
+                 (not (memq (process-status buf-process) '(run stop open listen)))
+                 (not (process-query-on-exit-flag buf-process))))
+        (eq buf-mode 'dired-mode)
+        (not (equal "TAGS" buf-name))
+        (not (buffer-modified-p buf)))))
