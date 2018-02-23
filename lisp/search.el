c021f4ad91b7c4cd6ba725415f8dac3c58e0e221
Stuart Campbell <stuart@harto.org>
2018-02-22 20:37:55 -0800

    project search improvements

--- lisp/search.el
+++ lisp/search.el
@@ -2,18 +2,18 @@
 
 ;; TODO: monorepo subproject support
 
-;; TODO: find symbol according to current mode (check if this needs fixing)
-;; TODO: optionally expand search to all filetypes in project
-;; TODO: fallback to various kinds of grep (rg, find|xargs)
 (defun grep-project-for-identifier (&optional identifier)
   "An alternative to `xref-find-references' that unconditionally uses git-grep."
-  (interactive (list (if (= (prefix-numeric-value current-prefix-arg) 4)
-                         (prompt-for-identifier (symbol-at-point))
-                       (symbol-at-point))))
+  (interactive (list ;; (if (= (prefix-numeric-value current-prefix-arg) 4)
+                     ;;     (prompt-for-identifier (if (symbol-at-point)
+                     ;;                                (symbol-name (symbol-at-point))
+                     ;;                              nil))
+                     ;;   (symbol-at-point))
+                     (symbol-at-point)))
   (let* ((identifier-regexp (format "\\b%s\\b" identifier))
          (git-path (if (= (prefix-numeric-value current-prefix-arg) 4)
-                       "*"
-                     (format "*.%s" (file-name-extension (buffer-file-name)))))
+                       "*"  ; FIXME: is this useful?
+                     (format "\"*.%s\"" (file-name-extension (buffer-file-name)))))
          (cmd (format "git --no-pager grep --color -n -e \"%s\" -- %s"
                       identifier-regexp
                       git-path))
@@ -21,10 +21,18 @@
     (grep cmd)))
 
 (defun prompt-for-identifier (default-identifier)
-  (read-input "Grep project for identifier: " default-identifier))
+  ;(message "default-identifier: %s (%s)" default-identifier (type-of default-identifier))
+  (read-string (format "Grep project for identifier%s: " (if default-identifier
+                                                             (format " (%s)" default-identifier) ""))
+               nil nil default-identifier))
 
 ;; (defadvice grep (before kill-grep-before-grep)
 ;;   (kill-grep))
 
 (eval-after-load 'find-things-fast
-  '(setq-default ftf-filetypes '("*")))
+  '(progn
+     (setq-default ftf-filetypes '("*"))
+     (advice-add 'ftf-get-find-command :around #'ftf-add-files-filter)))
+
+(defun ftf-add-files-filter (oldfun &rest args)
+  (concat (apply oldfun args) " -type f"))
