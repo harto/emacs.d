85e028dc41491d62e4892e4ffd380f7ef6fbc422
Stuart Campbell <stuart@harto.org>
2018-02-22 20:32:59 -0800

    Upgrade all the things

---
+++ vendor/elpa/rg-20180218.1129/rg-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rg" "20180218.1129" "A search tool based on ripgrep." '((cl-lib "0.5") (emacs "24") (s "1.10.0")) :commit "26413536ee71ac6ee07f193b94e60e544d800655" :url "https://github.com/dajva/rg.el" :keywords '("matching" "tools"))
