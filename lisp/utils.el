4904ff5976f688606e48c13edd6935ab8858aaca
Stuart Campbell <stuart@harto.org>
2018-03-02 20:59:11 -0800

    Drop various string helpers
    These are mostly provided as builtins now.

--- lisp/utils.el
+++ lisp/utils.el
@@ -1,25 +1,4 @@
 ;; =====================================
-;;  Strings
-
-(defun string-trim (s)
-  (replace-regexp-in-string "^[[:blank:]\n]+\\|[[:blank:]\n]+$" "" s))
-
-(defun string-unquote (start end)
-  "Removes quotation marks from start and end of each line in region."
-  (interactive "r")
-  (replace-regexp "^[[:space:]]*\"\\|\"[[:space:]]*\\(\\+\\|;\\)[[:space:]]*$"
-                  "" nil start end))
-
-(defun string-join (strings sep)
-  "Join a list of strings with a given separator."
-  (mapconcat #'identity strings sep))
-
-(defun string-drop-prefix (prefix s)
-  "Returns S without leading PREFIX."
-  (if (string-prefix-p prefix s)
-      (substring s (length prefix))))
-
-;; =====================================
 ;; Project-related helpers
 
 (defun project-directory ()
