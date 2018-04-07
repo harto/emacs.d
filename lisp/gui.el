24a698622fa8c15a8be19c225055d9eb2d107013
Stuart Campbell <stuart@harto.org>
2018-04-06 22:40:16 -0700

    Add load-theme hook

--- lisp/gui.el
+++ lisp/gui.el
@@ -1,18 +1,34 @@
 ;; GUI instance acts as server (should only be one)
 (server-start)
 
-;; Themes, fonts, etc.
+;; Theme hooks, per http://www.greghendershott.com/2017/02/emacs-themes.html
 
-(setq solarized-use-variable-pitch nil)
-(setq solarized-height-minus-1 1.0)
-(setq solarized-height-plus-1 1.0)
-(setq solarized-height-plus-2 1.0)
-(setq solarized-height-plus-3 1.0)
-(setq solarized-height-plus-4 1.0)
+(defvar load-theme-hooks nil)
+
+(defun load-theme-advice (f theme-id &optional no-confirm no-enable &rest args)
+  (unless no-enable
+    ;; disable all themes for blank slate
+    (mapc #'disable-theme custom-enabled-themes))
+  (prog1
+      (apply f theme-id no-confirm no-enable args)
+    (unless no-enable
+      (dolist (hook load-theme-hooks)
+        (funcall hook theme-id)))))
+
+(advice-add 'load-theme :around #'load-theme-advice)
+
+;; Theme configuration
 
 ;; Tweak modeline border, per
 ;; https://github.com/bbatsov/solarized-emacs#underline-position-setting-for-x
 (setq x-underline-at-descent-line t)
+
+(setq solarized-use-variable-pitch nil
+      solarized-height-minus-1 1.0
+      solarized-height-plus-1 1.0
+      solarized-height-plus-2 1.0
+      solarized-height-plus-3 1.0
+      solarized-height-plus-4 1.0)
 
 (load-theme 'solarized-dark)
 
