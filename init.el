5c19e0f4985a37c4719c77cbb85011fedabdbb6f
Stuart Campbell <stuart@harto.org>
2019-09-27 11:10:54 -0700

    Move packages to ./elpa

--- init.el
+++ init.el
@@ -1,13 +1,13 @@
 ;; ad hoc 3rd-party extensions
-(add-to-list 'load-path "~/.emacs.d/vendor")
-(let ((default-directory "~/.emacs.d/vendor"))
-  (normal-top-level-add-subdirs-to-load-path))
+;; (add-to-list 'load-path "~/.emacs.d/vendor")
+;; (let ((default-directory "~/.emacs.d/vendor"))
+;;   (normal-top-level-add-subdirs-to-load-path))
 
 ;; managed packages
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-unstable" . "https://melpa.org/packages/") t)
-(setq package-user-dir (expand-file-name "~/.emacs.d/vendor/elpa"))
+(setq package-user-dir (expand-file-name "~/.emacs.d/elpa"))
 (package-initialize)
 
 ;; customizations
