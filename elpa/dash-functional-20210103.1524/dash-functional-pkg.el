acd95ab1cbabe1d94a57d102f30620fa91d0e882
Stuart Campbell <stuart@harto.org>
2021-01-06 14:11:39 -0800

    lsp-mode

---
+++ elpa/dash-functional-20210103.1524/dash-functional-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/stuart/.emacs.d/elpa/dash-functional-20210103.1524/dash-functional.el  -*- no-byte-compile: t -*-
+(define-package "dash-functional" "20210103.1524" "Collection of useful combinators for Emacs Lisp" '((emacs "24") (dash "2.0.0")) :commit "5a64259a35d6b5dd6f02693de51e8a61fb342a0c" :keywords '("extensions" "lisp") :url "https://github.com/magnars/dash.el")
