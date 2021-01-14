2ba2cefcd1c2f1527e41c7b72593a617efa26ff5
Stuart Campbell <stuart@harto.org>
2021-01-14 09:23:08 -0800

    Move some early-init stuff back into normal init

--- early-init.el
+++ early-init.el
@@ -1,13 +1,3 @@
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://stable.melpa.org/packages/") t)
-(add-to-list 'package-archives '("melpa-unstable" . "https://melpa.org/packages/") t)
-(setq package-user-dir (expand-file-name "~/.emacs.d/elpa"))
-
-;; vendored packages
-;; (add-to-list 'load-path "~/.emacs.d/vendor")
-;; (let ((default-directory "~/.emacs.d/vendor"))
-;;   (normal-top-level-add-subdirs-to-load-path))
-
 ;; Disable unwanted chrome before it has the chance to appear
 (if (boundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (if (boundp 'tool-bar-mode) (tool-bar-mode -1))
--- init.el
+++ init.el
@@ -1,3 +1,8 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-unstable" . "https://melpa.org/packages/") t)
+(setq package-user-dir (expand-file-name "~/.emacs.d/elpa"))
+
 (setq custom-file "~/.emacs-custom.el")
 (load custom-file nil t)
 
