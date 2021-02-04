dd001f7cf177c35f015a69c373d54883f9d0e590
Stuart Campbell <stuart@harto.org>
2021-02-04 12:45:22 -0800

    package configuration into early-init.el

--- early-init.el
+++ early-init.el
@@ -3,7 +3,13 @@
 ;; collection. (The default is 800 kilobytes.) Measured in bytes.
 (setq gc-cons-threshold (* 100 1000 1000))
 
-;; Disable unwanted chrome before it has the chance to appear
+;; Disable unwanted chrome before it has the chance to appear.
 (if (boundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (if (boundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (boundp 'menu-bar-mode) (menu-bar-mode -1))
+
+;; Code that should run before `package-initialize' is meant to go in this file,
+;; according to the help documentation for that function.
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-unstable" . "https://melpa.org/packages/") t)
