cbe1eb97ab05aa24b1ce43fdf346720e1dab23ea
Stuart Campbell <stuart@harto.org>
2024-02-06 12:10:52 +1100

    Upgrade all dependencies

---
+++ elpa/eslintd-fix-20210731.1649/eslintd-fix-pkg.el
@@ -0,0 +1,13 @@
+(define-package "eslintd-fix" "20210731.1649" "use eslint_d to automatically fix js files"
+  '((dash "2.12.0")
+    (emacs "26.3"))
+  :commit "3897d8a679a6e98e3f5054aaefe07f6b55f8f128" :authors
+  '(("Aaron Jensen" . "aaronjensen@gmail.com"))
+  :maintainers
+  '(("Aaron Jensen" . "aaronjensen@gmail.com"))
+  :maintainer
+  '("Aaron Jensen" . "aaronjensen@gmail.com")
+  :url "https://github.com/aaronjensen/eslintd-fix")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
