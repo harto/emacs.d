9e4724cab74b91fe1aac3db1e849099c92c29046
Stuart Campbell <stuart@harto.org>
2024-10-08 10:23:06 +1100

    Upgrade all the things

---
+++ elpa/lsp-mode-20241004.151/lsp-mode-pkg.el
@@ -0,0 +1,15 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "lsp-mode" "20241004.151"
+  "LSP mode."
+  '((emacs         "27.1")
+    (dash          "2.18.0")
+    (f             "0.20.0")
+    (ht            "2.3")
+    (spinner       "1.7.3")
+    (markdown-mode "2.3")
+    (lv            "0")
+    (eldoc         "1.11"))
+  :url "https://github.com/emacs-lsp/lsp-mode"
+  :commit "3a952ca135bd508e7dee4650a195b7ec5886a0ea"
+  :revdesc "3a952ca135bd"
+  :keywords '("languages"))
