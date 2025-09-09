e9f283deec60b0d8731c5ca345868c646ab1ed9e
Stuart Campbell <stuart@harto.org>
2025-09-09 21:49:17 +1000

    Misc audit & cleanup

--- early-init.el
+++ early-init.el
@@ -1,4 +1,4 @@
-;; (from https://blog.d46.us/advanced-emacs-startup/)
+;; (From https://blog.d46.us/advanced-emacs-startup/)
 ;; Make startup faster by reducing the frequency of garbage
 ;; collection. (The default is 800 kilobytes.) Measured in bytes.
 (setq gc-cons-threshold (* 100 1000 1000))
@@ -17,8 +17,8 @@
                     :family "Roboto Mono"
                     :height sc/font-height-regular)
 
-;; Code that should run before `package-initialize' is meant to go in this file,
-;; according to the help documentation for that function.
+;; Code that should run before `package-initialize' is meant to go in this file
+;; (according to the help documentation for that function)
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-unstable" . "https://melpa.org/packages/") t)
