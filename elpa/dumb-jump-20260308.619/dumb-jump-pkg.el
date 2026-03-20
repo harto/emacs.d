b599fbe8ecccae507f8b2320f9abcd6907008586
Stuart Campbell <stuart@harto.org>
2026-03-20 20:49:13 +1100

    Upgrade dumb-jump

---
+++ elpa/dumb-jump-20260308.619/dumb-jump-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "dumb-jump" "20260308.619"
+  "Jump to definition for 60+ languages without configuration."
+  '((emacs "26.1"))
+  :url "https://github.com/jacktasia/dumb-jump"
+  :commit "a2285fc46c41b98c1eaf6904d58a0448cfdc920d"
+  :revdesc "a2285fc46c41"
+  :keywords '("programming"))
